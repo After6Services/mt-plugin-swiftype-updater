1412f4ef47486255a63d8298f0831522d5a833aa
Jay Allen <jay@endevver.com>
2018-01-18 00:47:55 -0800

    Reverting to old-style package declarations
    Package blocks weren’t introduced until after 5.10.

--- lib/SwiftypeUpdater/API.pm
+++ lib/SwiftypeUpdater/API.pm
@@ -1,176 +1,175 @@
-package SwiftypeUpdater::API {
+package SwiftypeUpdater::API;
 
-    use v5.10;
-    use Moo;
-    use strictures 2;
-    use namespace::clean;
-    use REST::Client;
-    use URI;
-    use JSON;
-    use Encode qw(encode_utf8);
-    use Data::Printer;
+use v5.10;
+use Moo;
+use strictures 2;
+use namespace::clean;
+use REST::Client;
+use URI;
+use JSON;
+use Encode qw(encode_utf8);
+use Data::Printer;
 
-    extends 'MT::ErrorHandler';
+extends 'MT::ErrorHandler';
 
-    has [qw( key engine_slug )] => (
-        is => 'lazy',
-        isa => sub {
-            die "'$_[0]' is not a string"
-                unless defined($_[0]) && length($_[0]) && $_[0] =~ m{[A-Za-z]+};
-        },
-        default => sub { '' },
-    );
+has [qw( key engine_slug )] => (
+    is => 'lazy',
+    isa => sub {
+        die "'$_[0]' is not a string"
+            unless defined($_[0]) && length($_[0]) && $_[0] =~ m{[A-Za-z]+};
+    },
+    default => sub { '' },
+);
 
-    has 'endpoint' => (
-        is      => 'lazy',
-        isa     => sub {
-            die "$_[0] is not an endpoint URL"
-                unless ref($_[0]) && $_[0]->isa('URI::http');
-        },
-        coerce  => sub { URI->new( $_[0] ) },
-        default => sub { URI->new('https://api.swiftype.com/api/v1') }
-    );
+has 'endpoint' => (
+    is      => 'lazy',
+    isa     => sub {
+        die "$_[0] is not an endpoint URL"
+            unless ref($_[0]) && $_[0]->isa('URI::http');
+    },
+    coerce  => sub { URI->new( $_[0] ) },
+    default => sub { URI->new('https://api.swiftype.com/api/v1') }
+);
 
-    has 'timeout' => (
-        is      => 'lazy',
-        default => sub { 10 },
-    );
+has 'timeout' => (
+    is      => 'lazy',
+    default => sub { 10 },
+);
 
-    has 'useragent' => (
-        is      => 'lazy',
-        default => sub { 'LWP::UserAgent' },
-        coerce => sub {
-            # say STDERR "In useragent coerce with $_[0]";
-            ref($_[0]) ? $_[0] : $_[0]->new;
-        }
-    );
-
-    has 'domain_id' => (
-        is      => 'lazy',
-    );
-
-    has 'client' => (
-        is  => 'lazy',
-        isa => sub {
-          die "$_[0] is not REST::Client instance"
-              unless ref($_[0]) && $_[0]->isa('REST::Client');
-        },
-    );
-
-    has 'debug' => (
-        is      => 'rw',
-        default => sub { 0 },
-    );
-
-    sub _build_client {
-        my $self   = shift;
-        my $client = REST::Client->new({
-            host      => $self->endpoint,
-            timeout   => $self->timeout,
-            useragent => $self->useragent,
-        });
-
-        $self->debug
-            and say STDERR "Created REST::Client instance ".np($client);
-
-        return $client;
+has 'useragent' => (
+    is      => 'lazy',
+    default => sub { 'LWP::UserAgent' },
+    coerce => sub {
+        # say STDERR "In useragent coerce with $_[0]";
+        ref($_[0]) ? $_[0] : $_[0]->new;
     }
+);
 
-    sub _build_domain_id {
-        my $self = shift;
-        my $path = sprintf( '/engines/%s/domains.json?auth_token=%s',
-                                $self->engine_slug, $self->key );
-        my $domain = decode_json(
-                    $self->client->GET($path)->responseContent() );
-        return $domain->[0]{id};
-    }
+has 'domain_id' => (
+    is      => 'lazy',
+);
 
-    sub crawl_url {
-        my $self   = shift;
-        my $url    = URI->new(+shift);
-        unless ( $url->isa( 'URI::http' )) {
-            my $msg = "Invalid URL in crawl_url: '$url'";
-            warn $msg;
-            return $self->error($msg);
-        }
+has 'client' => (
+    is  => 'lazy',
+    isa => sub {
+      die "$_[0] is not REST::Client instance"
+          unless ref($_[0]) && $_[0]->isa('REST::Client');
+    },
+);
 
-        my $path   = sprintf(
-            '/engines/%s/domains/%s/crawl_url.json'
-            , $self->engine_slug
-            , $self->domain_id
-        );
+has 'debug' => (
+    is      => 'rw',
+    default => sub { 0 },
+);
 
-        my $data = {
-            # auth_token => $self->key,
-            url        => $url->as_string,
-        };
-        my $encoded_data = encode_utf8(encode_json($data));
+sub _build_client {
+    my $self   = shift;
+    my $client = REST::Client->new({
+        host      => $self->endpoint,
+        timeout   => $self->timeout,
+        useragent => $self->useragent,
+    });
 
-        my $h = HTTP::Headers->new();
-        $h->authorization_basic( $self->key );
-        $h->content_type('application/json; charset=UTF-8');
-        my %headers = $h->flatten;
+    $self->debug
+        and say STDERR "Created REST::Client instance ".np($client);
 
-        if ( my $db = $self->debug ) {
-            say STDERR "crawl_url API call: PUT $path with body "
-                        .np($data);
-            p %headers;
-            return 1 if $db =~ m{NoCalls(\b|Crawl)}i;
-        }
+    return $client;
+}
 
-        my $response = $self->client->PUT( $path, $encoded_data, \%headers );
-        $self->debug and say STDERR $response->responseContent();
+sub _build_domain_id {
+    my $self = shift;
+    my $path = sprintf( '/engines/%s/domains.json?auth_token=%s',
+                            $self->engine_slug, $self->key );
+    my $domain = decode_json(
+                $self->client->GET($path)->responseContent() );
+    return $domain->[0]{id};
+}
 
-        return 1 if $response->responseCode() == 200;
-
-        my $msg
-            = sprintf "ERROR: crawl_url of %s failed with %s error: %s\n",
-                $url->as_string,
-                $response->responseCode(),
-                $response->responseContent();
+sub crawl_url {
+    my $self   = shift;
+    my $url    = URI->new(+shift);
+    unless ( $url->isa( 'URI::http' )) {
+        my $msg = "Invalid URL in crawl_url: '$url'";
         warn $msg;
-        return $self->error( $msg );
+        return $self->error($msg);
     }
 
-    sub destroy_url {
-        my $self   = shift;
-        my $url    = URI->new(+shift);
-        unless ( $url->isa( 'URI::http' )) {
-            my $msg = "Invalid URL in destroy_url: '$url'";
-            warn $msg;
-            return $self->error($msg);
-        }
+    my $path   = sprintf(
+        '/engines/%s/domains/%s/crawl_url.json'
+        , $self->engine_slug
+        , $self->domain_id
+    );
 
-        my $path   = sprintf(
-            '/engines/%s/document_types/page/documents/destroy_url'
-            .'?url=%s'
-            , $self->engine_slug
-            , $url->as_string
-        );
+    my $data = {
+        # auth_token => $self->key,
+        url        => $url->as_string,
+    };
+    my $encoded_data = encode_utf8(encode_json($data));
 
-        my $h = HTTP::Headers->new();
-        $h->authorization_basic( $self->key );
-        # $h->content_type('application/json; charset=UTF-8');
-        my %headers = $h->flatten;
+    my $h = HTTP::Headers->new();
+    $h->authorization_basic( $self->key );
+    $h->content_type('application/json; charset=UTF-8');
+    my %headers = $h->flatten;
 
-        if ( my $db = $self->debug ) {
-            say STDERR "destroy_url API call: DELETE $path";
-            p %headers;
-            return 1 if $db =~ m{NoCalls(\b|Destroy)}i;
-        }
+    if ( my $db = $self->debug ) {
+        say STDERR "crawl_url API call: PUT $path with body "
+                    .np($data);
+        p %headers;
+        return 1 if $db =~ m{NoCalls(\b|Crawl)}i;
+    }
 
-        my $response = $self->client->DELETE($path, \%headers);
-        $self->debug and say STDERR $response->responseContent();
+    my $response = $self->client->PUT( $path, $encoded_data, \%headers );
+    $self->debug and say STDERR $response->responseContent();
 
-        return 1 if $response->responseCode() == 200;
+    return 1 if $response->responseCode() == 200;
 
-        my $msg = sprintf "ERROR: destroy_url of %s failed with %s error: %s\n",
-                        $url->as_string,
-                        $response->responseCode(),
-                        $response->responseContent();
+    my $msg
+        = sprintf "ERROR: crawl_url of %s failed with %s error: %s\n",
+            $url->as_string,
+            $response->responseCode(),
+            $response->responseContent();
+    warn $msg;
+    return $self->error( $msg );
+}
+
+sub destroy_url {
+    my $self   = shift;
+    my $url    = URI->new(+shift);
+    unless ( $url->isa( 'URI::http' )) {
+        my $msg = "Invalid URL in destroy_url: '$url'";
         warn $msg;
-        return $self->error( $msg );
+        return $self->error($msg);
     }
+
+    my $path   = sprintf(
+        '/engines/%s/document_types/page/documents/destroy_url'
+        .'?url=%s'
+        , $self->engine_slug
+        , $url->as_string
+    );
+
+    my $h = HTTP::Headers->new();
+    $h->authorization_basic( $self->key );
+    # $h->content_type('application/json; charset=UTF-8');
+    my %headers = $h->flatten;
+
+    if ( my $db = $self->debug ) {
+        say STDERR "destroy_url API call: DELETE $path";
+        p %headers;
+        return 1 if $db =~ m{NoCalls(\b|Destroy)}i;
+    }
+
+    my $response = $self->client->DELETE($path, \%headers);
+    $self->debug and say STDERR $response->responseContent();
+
+    return 1 if $response->responseCode() == 200;
+
+    my $msg = sprintf "ERROR: destroy_url of %s failed with %s error: %s\n",
+                    $url->as_string,
+                    $response->responseCode(),
+                    $response->responseContent();
+    warn $msg;
+    return $self->error( $msg );
 }
 
 1;
--- lib/SwiftypeUpdater/Worker/Update.pm
+++ lib/SwiftypeUpdater/Worker/Update.pm
@@ -1,100 +1,99 @@
-package SwiftypeUpdater::Worker::Update {
+package SwiftypeUpdater::Worker::Update;
 
-    use strict;
-    use v5.10;
-    use TheSchwartz::Job;
-    use MT::TheSchwartz;
+use strict;
+use v5.10;
+use TheSchwartz::Job;
+use MT::TheSchwartz;
 
-    sub grab_for    { 60 }
-    sub max_retries { 100000 }
-    sub retry_delay { 60 }
+sub grab_for    { 60 }
+sub max_retries { 100000 }
+sub retry_delay { 60 }
 
-    sub work {
-        my $class                = shift;
-        my TheSchwartz::Job $job = shift;
-        my $mt                   = MT->instance;
+sub work {
+    my $class                = shift;
+    my TheSchwartz::Job $job = shift;
+    my $mt                   = MT->instance;
 
-        my @jobs = _find_coalescing_jobs( $job );
+    my @jobs = _find_coalescing_jobs( $job );
 
-        my $cfg  = MT::ConfigMgr->instance;
-        my $st   = SwiftypeUpdater::API->new(
-            endpoint    => $cfg->SwiftypeAPIEndpoint,
-            key         => $cfg->SwiftypeAPIKey,
-            engine_slug => $cfg->SwiftypeEngineSlug,
-            domain_id   => $cfg->SwiftypeDomainID,
-            timeout     => $cfg->SwiftypeTimeout,
-            useragent   => MT->instance->new_ua({
-                            timeout => $cfg->SwiftypeTimeout }),
-            debug       => $cfg->SwiftypeDebug,
-        );
+    my $cfg  = MT::ConfigMgr->instance;
+    my $st   = SwiftypeUpdater::API->new(
+        endpoint    => $cfg->SwiftypeAPIEndpoint,
+        key         => $cfg->SwiftypeAPIKey,
+        engine_slug => $cfg->SwiftypeEngineSlug,
+        domain_id   => $cfg->SwiftypeDomainID,
+        timeout     => $cfg->SwiftypeTimeout,
+        useragent   => MT->instance->new_ua({
+                        timeout => $cfg->SwiftypeTimeout }),
+        debug       => $cfg->SwiftypeDebug,
+    );
 
-        my @urls;
-        foreach my $job ( @jobs ) {
-            my ( $method, $url ) = _url_from_job( $job );
-            next unless $url;
+    my @urls;
+    foreach my $job ( @jobs ) {
+        my ( $method, $url ) = _url_from_job( $job );
+        next unless $url;
 
-            if ( $st->$method( $url )) {
-                $job->completed();
-            }
-            else {
-                $job->failed( 'Error during recrawl: ' . $st->errstr );
-            }
-        }
-    }
-
-    sub _find_coalescing_jobs {
-        my $job  = shift;
-        my @jobs = ( $job );
-        if ( my $key = $job->coalesce ) {
-            my $ts   = MT::TheSchwartz->instance;
-            my $func = $ts->can('find_job_with_coalescing_value');
-            while ( my $job = $func->( $ts, __PACKAGE__, $key )) {
-                push @jobs, $job;
-            }
-        }
-        return @jobs
-    }
-
-    sub _url_from_job {
-        my $job = shift;
-        my ( $method, $url );
-        if ( $job->uniqkey =~ m{^delete} ) {
-            $method = 'destroy_url';
-            my $arg       = $job->arg;
-            my $file      = $arg->{file};
-            my $entry     = $arg->{entry};
-            my $blog      = $entry->blog;
-            my $permalink = $entry->permalink;
-            ...;
+        if ( $st->$method( $url )) {
+            $job->completed();
         }
         else {
-            $method = 'crawl_url';
-            $url = $job->arg;
-            unless ( $url ) {
-                my $fi = MT->model('fileinfo')->load( $job->uniqkey )
-                    or $job->completed();
-                if ( $fi and $url = _url_from_fileinfo( $fi )) {
-                    $job->permanent_failure( "No URL for file: "
-                                             . $fi->file_path );
-                }
+            $job->failed( 'Error during recrawl: ' . $st->errstr );
+        }
+    }
+}
+
+sub _find_coalescing_jobs {
+    my $job  = shift;
+    my @jobs = ( $job );
+    if ( my $key = $job->coalesce ) {
+        my $ts   = MT::TheSchwartz->instance;
+        my $func = $ts->can('find_job_with_coalescing_value');
+        while ( my $job = $func->( $ts, __PACKAGE__, $key )) {
+            push @jobs, $job;
+        }
+    }
+    return @jobs
+}
+
+sub _url_from_job {
+    my $job = shift;
+    my ( $method, $url );
+    if ( $job->uniqkey =~ m{^delete} ) {
+        $method = 'destroy_url';
+        my $arg       = $job->arg;
+        my $file      = $arg->{file};
+        my $entry     = $arg->{entry};
+        my $blog      = $entry->blog;
+        my $permalink = $entry->permalink;
+        ...;
+    }
+    else {
+        $method = 'crawl_url';
+        $url = $job->arg;
+        unless ( $url ) {
+            my $fi = MT->model('fileinfo')->load( $job->uniqkey )
+                or $job->completed();
+            if ( $fi and $url = _url_from_fileinfo( $fi )) {
+                $job->permanent_failure( "No URL for file: "
+                                         . $fi->file_path );
             }
         }
-        return $url;
     }
+    return $url;
+}
 
-    sub _url_from_fileinfo {
-        my $fi        = shift or return;
-        my $blog      = MT->model('blog')->load( $fi->blog_id );
-        my $base_url  = $blog->site_url;
-        $base_url    .= '/' unless $base_url =~ m|/$|;
-        my $url       = $base_url . $fi->url;
-        $url          =~ s{(?<!:)//+}{/}g;
-        MT::TheSchwartz->debug(
-            $url ? "Constructed URL: $url"
-                 : "Warning: couldn't locate URL for file: " . $fi->file_path
-        );
-        return $url;
-    }
+sub _url_from_fileinfo {
+    my $fi        = shift or return;
+    my $blog      = MT->model('blog')->load( $fi->blog_id );
+    my $base_url  = $blog->site_url;
+    $base_url    .= '/' unless $base_url =~ m|/$|;
+    my $url       = $base_url . $fi->url;
+    $url          =~ s{(?<!:)//+}{/}g;
+    MT::TheSchwartz->debug(
+        $url ? "Constructed URL: $url"
+             : "Warning: couldn't locate URL for file: " . $fi->file_path
+    );
+    return $url;
 }
 
 1;
